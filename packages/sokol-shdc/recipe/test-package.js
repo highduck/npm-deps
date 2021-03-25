060884d4b8a310ce25ebdb9a7236ac22c7901273
ilyak <ilyak@playtika.com>
2021-03-25 11:14:36 +0300

    sokol-shdc: enable windows

--- packages/sokol-shdc/recipe/test-package.js
+++ packages/sokol-shdc/recipe/test-package.js
@@ -11,13 +11,6 @@ catch {
     process.exit(-1);
 }
 
-// we are in local environment, so use `yarn bin` (yarn v1) to find local package executable
-// let bin = spawn.sync("yarn", ["bin", "sokol-shdc"], {encoding:'utf8'}).stdout;
-// if(bin != null) {
-//     bin = bin.split("\n").join("");
-// }
-// console.info("sokol-shdc.js path:", bin);
-
 const r = spawn.sync("sokol-shdc", [
     "-i", "test/simple2d.glsl",
     "-o", "build/test-shader/simple2d_shader.h",
@@ -36,13 +29,7 @@ if(r.status !== 0 && r.status !== 0xFFFFFFFF) {
 
 if(!existsSync("build/test-shader/simple2d_shader.h")) {
     console.error("shader header not found");
-    if(process.platform === 'win32') {
-        // TODO: fix win32 test
-        // bypass test for windows
-    }
-    else {
-        process.exit(-1);
-    }
+    process.exit(-1);
 }
 
 rmSync("build/test-shader", {recursive: true});
